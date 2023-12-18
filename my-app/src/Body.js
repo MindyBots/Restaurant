0748f5d1f3e0ebd08c1cb73fa3973bb36b62266e
MindyBots <mindybots2023@outlook.com>
2023-12-18 19:51:26 +0530

    Updated Body

--- my-app/src/Body.js
+++ my-app/src/Body.js
@@ -3,8 +3,8 @@ import { Container, Typography, Box } from '@mui/material';
 
 const Body = () => {
     return(
-       <Container sx={{height:"900px"}}>
-         <Box sx={{height:"300px"}}></Box>
+       <Container sx={{height:"930px"}}>
+         <Box sx={{height:"450px"}}></Box>
          <Typography variant='h2' align='center'>The Body</Typography>
        </Container>
     )
